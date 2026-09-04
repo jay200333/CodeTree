43161ab4a103e9f175d2569f6b30014294f2ad9c
jay200333 <mk43313@gmail.com>
2026-09-04 09:49:12 +0900

    [Passed] 함수를 이용한 연속부분수열 여부 판단하기 (56ms, 17MB)

---
+++ trail2/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,14 @@
+n1, n2 = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+# Please write your code here.
+def is_contiguous_subarray(n1, n2, a, b):
+    for i in range(n1 - n2 + 1):
+        if a[i : i + n2] == b:
+            return True
+    return False
+if is_contiguous_subarray(n1, n2, a, b):
+    print("Yes")
+else:
+    print("No")
