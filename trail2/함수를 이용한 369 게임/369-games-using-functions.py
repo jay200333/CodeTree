1ca1a4c4a38cd335b2936bc95f6b1976a81c9251
jay200333 <mk43313@gmail.com>
2026-09-04 09:05:30 +0900

    [Passed] 함수를 이용한 369 게임 (102ms, 17MB)

---
+++ trail2/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,8 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+answer = 0
+for i in range(a, b+1):
+    if '3' in str(i) or '6' in str(i) or '9' in str(i) or i % 3 == 0:
+        answer += 1
+print(answer)
