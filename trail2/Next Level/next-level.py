b04a5ab54c9e945e7cc204d7ec96cc0cfe503f79
jay200333 <mk43313@gmail.com>
2026-09-06 12:54:56 +0900

    [Passed] Next Level (62ms, 16MB)

---
+++ trail2/Next Level/next-level.py
@@ -0,0 +1,9 @@
+user2_id, user2_level = input().split()
+user2_level = int(user2_level)
+
+# Please write your code here.
+info1 = ("codetree", 10)
+info2 = (user2_id, user2_level)
+
+print(f"user {info1[0]} lv {info1[1]}")
+print(f"user {info2[0]} lv {info2[1]}")
