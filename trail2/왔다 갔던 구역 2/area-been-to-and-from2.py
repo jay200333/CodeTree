87295dad742571424ec466f7440855299cffb206
jay200333 <mk43313@gmail.com>
2026-09-09 21:40:06 +0900

    [Passed] 왔다 갔던 구역 2 (54ms, 16MB)

---
+++ trail2/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,32 @@
+n = int(input())
+x = []
+dir = []
+temp = []
+for _ in range(n):
+    xi, di = input().split()
+    x.append(int(xi))
+    dir.append(di)
+
+# Please write your code here.
+visited = [0] * 2001
+cur_idx = 100
+for i in range(n):
+    if dir[i] == 'R':
+        left = cur_idx
+        right = cur_idx + x[i]
+        cur_idx += x[i]
+    else:
+        left = cur_idx - x[i]
+        right = cur_idx
+        cur_idx -= x[i]
+    temp.append((left, right))
+
+for left, right in temp:
+    for j in range(left, right):
+        visited[j+100] += 1
+        
+answer = 0
+for i in visited:
+    if i >= 2:
+        answer += 1
+print(answer)
