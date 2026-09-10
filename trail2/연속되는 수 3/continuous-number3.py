62ab884d47aaa5d37f030d2dea7579f079623591
jay200333 <mk43313@gmail.com>
2026-09-10 11:26:28 +0900

    [Passed] 연속되는 수 3 (73ms, 17MB)

--- trail2/연속되는 수 3/continuous-number3.py
+++ trail2/연속되는 수 3/continuous-number3.py
@@ -2,12 +2,12 @@ N = int(input())
 arr = [int(input()) for _ in range(N)]
 
 # Please write your code here.
-answer = 1
+answer, count = 0, 0
 for i in range(N):
-    if i == 0 or ( arr[i] < 0 and arr[i-1] > 0) or ( arr[i] > 0 and arr[i-1] < 0):
-        count = 1
+    if i >= 1 and (arr[i] * arr[i-1] > 0):
+        count += 1
     else:
-        count += 1 
-        answer = max(count, answer)
+        count = 1 
+    answer = max(count, answer)
 
 print(answer)
