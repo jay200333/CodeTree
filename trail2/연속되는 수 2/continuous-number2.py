6ce522f8b1308e94523d6468420136b1bb9020c3
jay200333 <mk43313@gmail.com>
2026-09-10 10:02:49 +0900

    [Passed] 연속되는 수 2 (70ms, 17MB)

---
+++ trail2/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+answer = 1
+
+for i in range(n):
+    if i == 0 or arr[i] != arr[i-1]:
+        count = 1
+    else:
+        count += 1
+        answer = max(answer, count)
+
+print(answer)
